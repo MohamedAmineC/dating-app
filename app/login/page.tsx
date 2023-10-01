de83c7109e3b34162c2de4721e53baf020e8f982
Mohmaed Amine Chalouati <mohamedaminechalouati@gmail.com>
2023-10-01 13:32:09 +0200

    changing favicon and configuring the loading to be in the login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,12 +5,29 @@ import Logo from "../components/Logo";
 import SocialLogin from "../components/SocialLogin";
 import { FaFacebookF, FaGithub, FaGoogle } from "react-icons/fa";
 import Button from "../components/Button";
+import { useEffect, useState } from "react";
 
 const Page = () => {
     const router = useRouter();
+    const [loading,setLoading] = useState(true)
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setLoading(false)
+        }, 5000)
+        return () => clearTimeout(timeout)
+    })
   return (
-    <div className="h-full w-full bg-primarry grid place-items-center">
-        <div className="flex flex-col gap-8 items-center">
+
+    <div className={`h-full w-full grid place-items-center
+    ${!loading && 'bg-primarry'}`}>
+        {loading ? (
+            <Logo 
+            loading
+            primary={false}
+            />
+        ) : (
+        <>
+        <div className="flex flex-col gap-8 items-center" >
             <Logo 
             primary
             />
@@ -18,12 +35,16 @@ const Page = () => {
                 <Button 
                 actionLabel="Créer un compte"
                 primary
-                onAction={() => {}}
+                onAction={() => {
+                    router.push('/register')
+                }}
                 />
                 <Button 
                 actionLabel="Se connecter"
                 outline
-                onAction={() => {}}
+                onAction={() => {
+                    router.push('/signin')
+                }}
                 />
             </div>
             <div className="flex items-center gap-4">
@@ -44,6 +65,9 @@ const Page = () => {
         <div className="self-end mb-4 text-white">
             © 2023 MeetUp. All rights reserved.
         </div>
+        </>
+           )}
+
     </div>
   )
 }
